dd03a8588b84e9e04977c3ab889b97ee22fde773
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-06-23 21:57:41 -0300

    second batch of completed exercises and part 2 of exercises

--- part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
+++ part01-Part01_18.SumOfThreeNumbers/src/main/java/SumOfThreeNumbers.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 public class SumOfThreeNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        // Write your program here
+        System.out.println("Give the first number:");
+        int first = Integer.valueOf(scan.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.valueOf(scan.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.valueOf(scan.nextLine());
+        
+        System.out.println("The sum of the numbers is " + (first + second + third));
 
     }
 }
--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -6,5 +6,27 @@ public class GiftTax {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Value of the gift?");
+        int gift = Integer.valueOf(scan.nextLine());
+        int exceed = 0;
+        
+        if (gift < 4999) {
+            System.out.println("No tax!");
+        } else if (gift >= 5000 && gift <= 24999) {
+            exceed = gift - 5000;
+            System.out.println("Tax: " + (100 + (exceed * 0.08)));
+        } else if (gift >= 25000 && gift <= 54999) {
+            exceed = gift - 25000;
+            System.out.println("Tax: " + (1700 + (exceed * 0.1)));
+        } else if (gift >= 55000 && gift <= 199999) {
+            exceed = gift - 55000;
+            System.out.println("Tax: " + (4700 + (exceed * 0.12)));
+        } else if (gift >= 200000 && gift < 999999) {
+            exceed = gift - 200000;
+            System.out.println("Tax: " + (22100 + (exceed * 0.15)));
+        } else if (gift >= 1000000) {
+            exceed = gift - 1000000;
+            System.out.println("Tax: " + (142100 + (exceed * 0.17)));
+        }
     }
 }
